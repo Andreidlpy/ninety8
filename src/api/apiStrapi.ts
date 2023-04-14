a454da48372a23090833f82965844e3534cc786b
Oscar <dyausinoh@gmail.com>
2023-04-13 21:26:51 -0500

    api baseUrl updated

--- src/api/apiStrapi.ts
+++ src/api/apiStrapi.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiStrapi = axios.create( {
-    baseURL: 'http://localhost:8081/api',
+    baseURL: 'https://climaapp-production.up.railway.app/api',
     withCredentials: true
 })
 
